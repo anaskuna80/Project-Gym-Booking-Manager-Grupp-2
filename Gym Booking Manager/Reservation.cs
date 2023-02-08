bcb545490aac2006efd1e2e60514a6df7002f207
Alex Malik <alex.malik@outlook.com>
2023-02-08 08:56:25 +0100

    Uppdaterat kod för att innehålla start och enddate

--- Gym Booking Manager/Reservation.cs
+++ Gym Booking Manager/Reservation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gym_Booking_Manager.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,13 @@ namespace Gym_Booking_Manager
 {
     internal class Reservation
     {
+        public readonly DateTime StartDate;
+        public readonly DateTime EndDate;
+
+        public Reservation(DateTime startDate, DateTime endDate)
+        {
+            this.StartDate = startDate;
+            this.EndDate = endDate;
+        }
     }
 }
