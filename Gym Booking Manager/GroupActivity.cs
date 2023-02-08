95e18fb31b2f399384dde41d0c9ea9e3d0d7e69d
Emil Edman <emil_a.m.e@hotmail.com>
2023-02-08 11:03:01 +0100

    Added GroupActivity

--- Gym Booking Manager/GroupActivity.cs
+++ Gym Booking Manager/GroupActivity.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Gym_Booking_Manager
 {
-    internal class Class1
+    internal class GroupActivty
     {
+        private string ActivityID;
+        private int participantLimit;
+        private List<User> participants;
+        private Space space;
+        private Equipment equipment;
+        private string instructor;
+        private string timeSlot;
+
+        public void SignUp(User participant)
+        {
+            this.participants.Add(participant);
+        }
+
+        public void Modify()
+        {
+
+        }
     }
 }
