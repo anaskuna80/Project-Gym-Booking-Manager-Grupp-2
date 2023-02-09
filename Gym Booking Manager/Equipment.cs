b47f1fc271c9669dd0182f5a2a72034c0e3711be
Andreas K <andreas.kuylenstierna@gmail.com>
2023-02-09 09:44:36 +0100

    Reserv equipment and PersonalTrainer

--- Gym Booking Manager/Equipment.cs
+++ Gym Booking Manager/Equipment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gym_Booking_Manager.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,15 +7,54 @@ using System.Threading.Tasks;
 
 namespace Gym_Booking_Manager
 {
-    internal class Equipment
+    internal class Equipment : Calendar
     {
-        private String name;
+        /*private String name;
         private Calendar calendar;
 
         public Equipment(string name)
         {
             this.name = name;
             this.calendar = new Calendar();
-        }   
+        }*/
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public Calendar Calendar { get; set; }
+
+        public Equipment(string name, int quantity)
+        {
+            Name = name;
+            Quantity = quantity;
+            Calendar = new Calendar();
+        }
+
+        public void BookEquipment(IReservingEntity owner, DateTime startTime, DateTime endTime)
+        {
+            if (Quantity > 0 && Calendar.IsAvailable(startTime, endTime))
+            {
+                Quantity--;
+                Calendar.MakeReservation(owner, startTime, endTime);
+                Console.WriteLine("Equipment '{0}' has been successfully booked.", Name);
+            }
+            else
+            {
+                Console.WriteLine("Equipment '{0}' is not available for booking.", Name);
+            }
+        }
+
+        public bool CancelBooking(Reservation reservation)
+        {
+            if (Calendar.HasReservation(reservation))
+            {
+                Calendar.RemoveReservation(reservation);
+                Quantity++;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("This reservation does not exist for this equipment.");
+                return false;
+            }
+        }
     }
 }
