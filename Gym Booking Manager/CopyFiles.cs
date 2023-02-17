09c6be064fe959c21e9768d02111fc214a1358f7
Alex Malik <alex.malik@outlook.com>
2023-02-17 13:21:54 +0100

    Code

--- Gym Booking Manager/CopyFiles.cs
+++ Gym Booking Manager/CopyFiles.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace Gym_Booking_Manager
+
+public static class CsvFileManager
 {
-    internal class CopyFiles
+    public static void CopyCsvFilesToStorageFolder()
     {
+        // Get the path of the project folder
+        string projectPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+        // Get the path of the storage folder
+        string storagePath = Path.Combine(projectPath, "bin", "Debug", "net6.0", "GymDB", "storage");
+
+        // Get all CSV files in the project folder
+        string[] csvFiles = Directory.GetFiles(projectPath, "*.csv");
+
+        // Copy all CSV files to the storage folder
+        foreach (string csvFile in csvFiles)
+        {
+            string fileName = Path.GetFileName(csvFile);
+            string destFile = Path.Combine(storagePath, fileName);
+            File.Copy(csvFile, destFile, true);
+        }
     }
 }
+
+
+
+
+
