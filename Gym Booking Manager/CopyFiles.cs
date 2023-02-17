887c393aa43f3f0d1bf1e5db096ecb7564ac0a84
Alex Malik <alex.malik@outlook.com>
2023-02-17 15:32:14 +0100

    Updated code to check for files first.

--- Gym Booking Manager/CopyFiles.cs
+++ Gym Booking Manager/CopyFiles.cs
@@ -1,35 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
-
 
 public static class CsvFileManager
 {
     public static void CopyCsvFilesToStorageFolder()
     {
-        // Get the path of the project folder
-        string projectPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-
         // Get the path of the storage folder
-        string storagePath = Path.Combine(projectPath, "bin", "Debug", "net6.0", "GymDB", "storage");
+        string storagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "storage");
 
-        // Get all CSV files in the project folder
-        string[] csvFiles = Directory.GetFiles(projectPath, "*.csv");
-
-        // Copy all CSV files to the storage folder
-        foreach (string csvFile in csvFiles)
+        // Check if CSV files exist in the Gym Booking Manager\bin\Debug\net6.0 folder
+        string gymBookingManagerPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..");
+        string debugPath = Path.Combine(gymBookingManagerPath, "bin", "Debug", "net6.0","GymDB","storage");
+        string[] csvFileNames = new string[] { "calender.csv", "customer.csv", "groupactivities.csv", "Largeequipment.csv", "PersonalTrainer.csv", "space.csv", "sportsequipment.csv", "staff.csv" };
+        bool allFilesExist = true;
+        foreach (string csvFileName in csvFileNames)
         {
-            string fileName = Path.GetFileName(csvFile);
-            string destFile = Path.Combine(storagePath, fileName);
-            File.Copy(csvFile, destFile, true);
+            string csvFilePath = Path.Combine(debugPath, csvFileName);
+            if (!File.Exists(csvFilePath))
+            {
+                Console.WriteLine($"CSV file {csvFileName} does not exist in {debugPath} folder. Copying.....");
+                allFilesExist = false;
+            }
+        }
+
+        // Copy CSV files to the Gym Booking Manager\bin\Debug\net6.0 folder from storage folder if they do not exist
+        if (!allFilesExist)
+        {
+            foreach (string csvFileName in csvFileNames)
+            {
+                string storageFilePath = Path.Combine(storagePath, csvFileName);
+                string destFile = Path.Combine(debugPath, csvFileName);
+                File.Copy(storageFilePath, destFile, true);
+            }
+        }
+
+        // Copy CSV files to the storage folder
+        foreach (string csvFileName in csvFileNames)
+        {
+            string debugFilePath = Path.Combine(debugPath, csvFileName);
+            string destFile = Path.Combine(storagePath, csvFileName);
+            File.Copy(debugFilePath, destFile, true);
         }
     }
 }
-
-
-
-
-
