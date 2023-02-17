a1b35656a7d8ff89f5befa882d076e2e1a6449c8
Alex Malik <alex.malik@outlook.com>
2023-02-17 14:29:37 +0100

    added GroupActivityTests

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,46 +1,98 @@
-﻿using Gym_Booking_Manager;
-using static Gym_Booking_Manager.Space;
-using System;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
+using Gym_Booking_Manager;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Tests
+namespace Gym_Booking_Manager_Tests
 {
     [TestClass]
-    public class Tests
+    public class GroupActivityTests
     {
         [TestMethod]
-        public void GroupActivityAdded()
+        public void SignUp_AddsParticipantToList()
         {
-            GymDatabaseContext db = new GymDatabaseContext();
-            GroupSchedule schedule = new GroupSchedule(); // Do we need constructor arguments? Maybe GroupSchedule is static - meaning we don't need to call 'new'.
+            // Arrange
+            var groupActivity = new GroupActivity("Test Activity", 10, "Test Instructor", 1);
 
-            // // Some examples - or maybe even overloading of AddActivity()?
+            // Act
+            groupActivity.SignUp(1);
 
-            //GroupActivity activity = new GroupActivity(); // Args
-            //schedule.AddActivity(activity);
+            // Assert
+            Assert.AreEqual(1, groupActivity.participants.Count);
+            Assert.AreEqual(1, groupActivity.participants[0]);
+        }
 
-            // // struct ActivityDetails maybe? Letting the method generate the object to insert in its internal list
-            //ActivityDetails details = new ActivityDetails() // Args
-            //schedule.AddActivity(details);
+        [TestMethod]
+        public void SignUp_WhenActivityIsFull_DoesNotAddParticipantToList()
+        {
+            // Arrange
+            var groupActivity = new GroupActivity("Test Activity", 2, "Test Instructor", 1);
+            groupActivity.SignUp(1);
+            groupActivity.SignUp(2);
 
-            // // All the arguments?
-            // 
-            //schedule.AddActivity(participantLimit = ??, timeSlot = ??, instructor = ??, space = ??, ...)
+            // Act
+            groupActivity.SignUp(3);
 
-            // Persistance of the activity in the database should happen at some point as part class methods,
-            // and not as a db call in this scope. We call db.Read() as part of the test.
+            // Assert
+            Assert.AreEqual(2, groupActivity.participants.Count);
+            Assert.IsFalse(groupActivity.participants.Contains(3));
+        }
 
-            List<GroupActivity> persistedActivities = db.activities.Read<GroupActivity>(); // Make changes if necessary
+        
 
-            Assert.IsTrue(persistedActivities.Exists(x => ??));
-            // Replace ?? with a reasonable comparison.
-            // Example: if ?? is replaced with 'x == activity':
-            //     Assert.IsTrue(persistedActivities.Exists(x => x == activity));
-            // or 'x == new GroupActivity(args)':
-            //     Assert.IsTrue(persistedActivities.Exists(x => x == new GroupActivity(args)));
-            // then the GroupActivity may need to implement IEquatable<GroupActivity> for that comparison.
-            //
-            // If you prefer, you can also change the Assert statement to something else.
+        [TestMethod]
+        public void CompareTo_WhenOtherIsNull_Returns1()
+        {
+            // Arrange
+            var groupActivity1 = new GroupActivity("Activity 1", 10, "Instructor 1", 1);
+            GroupActivity groupActivity2 = null;
+
+            // Act
+            var result = groupActivity1.CompareTo(groupActivity2);
+
+            // Assert
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void CompareTo_WhenOtherIsLess_ReturnsNegativeValue()
+        {
+            // Arrange
+            var groupActivity1 = new GroupActivity("Activity 1", 10, "Instructor 1", 1);
+            var groupActivity2 = new GroupActivity("Activity 2", 10, "Instructor 2", 2);
+
+            // Act
+            var result = groupActivity1.CompareTo(groupActivity2);
+
+            // Assert
+            Assert.IsTrue(result < 0);
+        }
+
+        [TestMethod]
+        public void CompareTo_WhenOtherIsGreater_ReturnsPositiveValue()
+        {
+            // Arrange
+            var groupActivity1 = new GroupActivity("Activity 1", 10, "Instructor 1", 1);
+            var groupActivity2 = new GroupActivity("Activity 2", 10, "Instructor 2", 2);
+
+            // Act
+            var result = groupActivity2.CompareTo(groupActivity1);
+
+            // Assert
+            Assert.IsTrue(result > 0);
+        }
+
+        [TestMethod]
+        public void CompareTo_WhenOtherIsEqual_ReturnsZero()
+        {
+            // Arrange
+            var groupActivity1 = new GroupActivity("Activity 1", 10, "Instructor 1", 1);
+            var groupActivity2 = new GroupActivity("Activity 2", 10, "Instructor 2", 1);
+
+            // Act
+            var result = groupActivity1.CompareTo(groupActivity2);
+
+            // Assert
+            Assert.AreEqual(0, result);
         }
     }
 }
