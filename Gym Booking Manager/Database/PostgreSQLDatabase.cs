02ca18e983d9df1393dfc57abbcac5a96148c8ea
emil <emil.o.jonsson@gmail.com>
2023-03-24 10:09:21 +0100

    Here comes the database for you my friends

---
+++ Gym Booking Manager/Database/PostgreSQLDatabase.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace Gym_Booking_Manager.Database
+{
+    internal class PostgreSQLDatabase
+    {
+        public static Dictionary<string, Type> TypeOfClass { get; } = new Dictionary<string, Type>
+        {
+            { "Animal", typeof(Animal) },
+            { "Birds", typeof (Birds) },
+            { "Human", typeof(Human) }
+        };
+        public static NpgsqlConnection getConnection()
+        {
+            return new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=LÖSENORD;Database=TestDB");
+        }
+        public static void testConnection()
+        {
+            using (NpgsqlConnection con = getConnection())
+            {
+                con.Open();
+                if (con.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("Connected");
+                }
+            }
+        }
+        public static void createRecord(object theObject)
+        {
+            using (NpgsqlConnection con = getConnection())
+            {
+                var type = theObject.GetType();
+                var tableName = TypeOfClass.FirstOrDefault(x => x.Value == type).Key;
+                string propertyName = string.Empty;
+                foreach (var property in type.GetProperties().OrderBy(p => p.Name))
+                {
+                    if (property.GetValue(theObject) != null)
+                    {
+                        propertyName = propertyName + property.Name + ",";
+                    }
+                }
+                propertyName = propertyName.TrimEnd(',');
+                string query = $@"INSERT INTO {tableName} ({propertyName}) VALUES({theObject.ToString()});";
+                NpgsqlCommand cmd = new NpgsqlCommand(query, con);
+                con.Open();
+                int n = cmd.ExecuteNonQuery();
+            }
+        }
+        public static object[] readRecord(string typeOfObject, int id)
+        {
+            using (NpgsqlConnection con = getConnection())
+            {
+                var query = $"SELECT * FROM {typeOfObject} WHERE ID = {id}";
+                NpgsqlCommand cmd = new NpgsqlCommand(query, con);
+                con.Open();
+                var dataReader = cmd.ExecuteReader();
+                dataReader.Read();
+                int columns = dataReader.FieldCount;
+                object[] values = new object[columns];
+                dataReader.GetValues(values);
+                return values;
+            }
+        }
+        public static void updateRecord(string tableName, int id, string attributeToEdit, string value)
+        {
+            Type typeOfObject = TypeOfClass[tableName];
+            var TypeOfAttribute = typeOfObject.GetProperty(attributeToEdit);
+            object convertedVariable = Convert.ChangeType(value, TypeOfAttribute.PropertyType);
+            string query = string.Empty;
+            using (NpgsqlConnection con = getConnection())
+            {
+                if (convertedVariable is string)
+                {
+                    query = $@"UPDATE {tableName} SET {attributeToEdit} = '{convertedVariable}' WHERE ID = {id};";
+                }
+                else
+                {
+                    query = $@"UPDATE {tableName} SET {attributeToEdit} = {convertedVariable} WHERE ID = {id};";
+                }
+                NpgsqlCommand cmd = new NpgsqlCommand(query, con);
+                con.Open();
+                int n = cmd.ExecuteNonQuery();
+            }
+        }
+        public static void deleteRecord(string tableName, int id)
+        {
+            using (NpgsqlConnection con = getConnection())
+            {
+                var query = $@"DELETE FROM {tableName} WHERE ID = {id};";
+                NpgsqlCommand cmd = new NpgsqlCommand(query, con);
+                con.Open();
+                int n = cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
